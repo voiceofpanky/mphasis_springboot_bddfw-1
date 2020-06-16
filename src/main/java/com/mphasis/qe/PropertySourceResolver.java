3262b9351eb134aae68614405f94ddc5714ba021
Dipanjan Chakraborty <dipanjan.it@gmail.com>
2020-06-16 12:53:23 -0400

    Author Added to Property Resolver

--- src/main/java/com/mphasis/qe/PropertySourceResolver.java
+++ src/main/java/com/mphasis/qe/PropertySourceResolver.java
@@ -3,6 +3,10 @@ package com.mphasis.qe;
 import lombok.Data;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+/**
+**@author Dipanjan Chakraborty
+*/
+
 
 @Data
 @Component
