74e01feea09f48fb539d0ee3ad283dd76833fb3f
voiceofpanky <voiceofpanky@gmail.com>
2021-07-15 12:21:50 -0700

    Added java elements and method comments as per review comments

--- src/test/java/com/mphasis/qe/utils/ApiUtil.java
+++ src/test/java/com/mphasis/qe/utils/ApiUtil.java
@@ -23,9 +23,9 @@ import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 import lombok.extern.slf4j.Slf4j;
 /**
-*  @author: Dipanjan Chakraborty
+ *  @author: Dipanjan Chakraborty
  *  Util c  lass to handle all API calls
-* */
+ * */
 
 @Slf4j
 @Component
@@ -42,7 +42,11 @@ public class ApiUtil {
 		builder.addFilter(reportFilter);
 		request = RestAssured.given().spec(builder.build());
 	}
-
+	/**
+	 * GET Request method with below parameters
+	 * @param url
+	 * @return
+	 */
 	public Response getReq(String url) {
 		try {
 			response = request.get(new URI(url));
@@ -52,7 +56,12 @@ public class ApiUtil {
 		}
 		return null;
 	}
-
+	/**
+	 * GET Request method with below parameters
+	 * @param url
+	 * @param queryParam
+	 * @return
+	 */
 	public Response get(String url, Map<String, String> queryParam) {
 		try {
 			request.queryParams(queryParam);
@@ -63,7 +72,12 @@ public class ApiUtil {
 		}
 		return null;
 	}
-
+	/**
+	 * GET Request method with below parameters
+	 * @param url
+	 * @param headers
+	 * @return
+	 */
 	public Response getReq(String url, Map<String, String> headers) {
 		request.headers(headers);
 		try {
@@ -74,7 +88,12 @@ public class ApiUtil {
 		}
 		return null;
 	}
-
+	/**
+	 * POST Request method with below parameters
+	 * @param url
+	 * @param jsonBody
+	 * @return
+	 */
 	public Response postReq(String url, String jsonBody) {
 		request.body(jsonBody);
 		try {
@@ -85,7 +104,13 @@ public class ApiUtil {
 		}
 		return null;
 	}
-
+	/** 
+	 * POST Request method with below parameters
+	 * @param url
+	 * @param headers
+	 * @param jsonBody
+	 * @return
+	 */
 	public Response postReq(String url, Map<String, String> headers, String jsonBody) {
 		request.headers(headers);
 		request.body(jsonBody);
@@ -98,9 +123,10 @@ public class ApiUtil {
 		}
 		return null;
 	}
-	
+
 	/**
-	 * 
+	 *  @author: Pankaj Sao Added new custom methods
+	 *  POST Request method with below parameters
 	 * @param url
 	 * @param headers
 	 * @param params
@@ -120,15 +146,17 @@ public class ApiUtil {
 		}
 		return null;
 	} 
-	
-/**
- * @param url
- * @param headers
- * @param params
- * @param cookies
- * @param jsonBody
- * @return
- */
+
+	/**
+	 * @author: Pankaj Sao Added new custom methods
+	 * POST Request method with below parameters
+	 * @param url
+	 * @param headers
+	 * @param params
+	 * @param cookies
+	 * @param jsonBody
+	 * @return
+	 */
 	public Response postReq(String url, Map<String, String> headers,Map<String, String> params, Cookies cookies,String jsonBody ) {
 		request.headers(headers);
 		request.body(jsonBody);
@@ -143,8 +171,13 @@ public class ApiUtil {
 		}
 		return null;
 	} 
-	
-	
+
+	/**
+	 * PUT Request method with below parameters
+	 * @param url
+	 * @param jsonBody
+	 * @return
+	 */
 	public Response putReq(String url, String jsonBody) {
 		request.body(jsonBody);
 		try {
@@ -155,7 +188,13 @@ public class ApiUtil {
 		}
 		return null;
 	}
-
+	/**
+	 * PUT Request method with below parameters
+	 * @param url
+	 * @param headers
+	 * @param jsonBody
+	 * @return
+	 */
 	public Response putReq(String url, Map<String, String> headers, String jsonBody) {
 		request.headers(headers);
 		request.body(jsonBody);
@@ -167,7 +206,11 @@ public class ApiUtil {
 		}
 		return null;
 	}
-
+	/**
+	 * DELETE Request method with below parameters
+	 * @param url
+	 * @return
+	 */
 	public Response deleteReq(String url) {
 		try {
 			Response response = request.delete(new URI(url));
@@ -177,7 +220,12 @@ public class ApiUtil {
 		}
 		return null;
 	}
-
+	/**
+	 * DELETE Request method with below parameters
+	 * @param url
+	 * @param headers
+	 * @return
+	 */
 	public Response deleteReq(String url, Map<String, String> headers) {
 		request.headers(headers);
 		try {
@@ -188,26 +236,35 @@ public class ApiUtil {
 		}
 		return null;
 	}
-
+	/**
+	 * GETAsync Request method with below parameters
+	 * @param url
+	 * @return
+	 */
 	public Response getAsyncReq(String url) {
 		try {
 			response = request.get(new URI(url));
 
 			with().pollInterval(1000, TimeUnit.MILLISECONDS).and().with().pollDelay(20, TimeUnit.MILLISECONDS)
-					.await("receiving response").atMost(180, TimeUnit.SECONDS).until(() -> response != null);
+			.await("receiving response").atMost(180, TimeUnit.SECONDS).until(() -> response != null);
 			return response;
 		} catch (URISyntaxException e) {
 			e.printStackTrace();
 		}
 		return null;
 	}
-
+	/**
+	 * GETAsync Request method with below parameters
+	 * @param url
+	 * @param headers
+	 * @return
+	 */
 	public Response getAsyncReq(String url, Map<String, String> headers) {
 		request.headers(headers);
 		try {
 			Response response = request.get(new URI(url));
 			with().pollInterval(1000, TimeUnit.MILLISECONDS).and().with().pollDelay(20, TimeUnit.MILLISECONDS)
-					.await("receiving response").atMost(180, TimeUnit.SECONDS).until(() -> response != null);
+			.await("receiving response").atMost(180, TimeUnit.SECONDS).until(() -> response != null);
 
 			return response;
 		} catch (URISyntaxException e) {
@@ -215,7 +272,12 @@ public class ApiUtil {
 		}
 		return null;
 	}
-
+	/**
+	 * POST Request method with below parameters
+	 * @param appRegisterUrl
+	 * @param requestBody
+	 * @return
+	 */
 	public Response postReq(String appRegisterUrl, JSONObject requestBody) {
 		request.body(requestBody.toString());
 		response = request.post(appRegisterUrl);
