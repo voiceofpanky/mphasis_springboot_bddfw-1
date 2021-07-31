f7966005855a69c789ecaa1020945a1e675284b5
voiceofpanky <voiceofpanky@gmail.com>
2021-07-31 01:24:07 -0700

    -Added testng dependency -Added ParallelRunner -Added feature scenario

--- src/test/java/com/mphasis/qe/runner/ParallelRunner.java
+++ src/test/java/com/mphasis/qe/runner/ParallelRunner.java
@@ -1,34 +1,35 @@
 package com.mphasis.qe.runner;
-
-import org.junit.runner.RunWith;
 import org.testng.annotations.DataProvider;
-
-import io.cucumber.junit.Cucumber;
-import io.cucumber.junit.CucumberOptions;
+import org.testng.annotations.Test;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+import io.cucumber.testng.FeatureWrapper;
+import io.cucumber.testng.PickleWrapper;
 
 /****************************************************************************************
  * @author Pankaj Sao
  ****************************************************************************************/
 
 @CucumberOptions(
-        features = {"src/test/resources/featurefiles"},
-        glue = {"com.mphasis.qe.utils",
-                "com.mphasis.qe.stepdefs"},
-        tags = {"@web"},
-        strict = true,
-
-        monochrome = true,
-        dryRun = false,
-        plugin = {"pretty",
-                "json:target/cucumber-reports/cucumber.json",
-                "html:target/cucumber-reports/cucumber-html-report",
-        })
-@RunWith(Cucumber.class)
+		features = {"src/test/resources/featurefiles"},
+		glue = {"com.mphasis.qe.utils",
+		"com.mphasis.qe.stepdefs"},
+		tags = {"@web and @mphasis"},
+		strict = true,
+		monochrome = true,
+		dryRun = false,
+		plugin = {"pretty",
+				"json:target/cucumber-reports/cucumber.json",
+				"html:target/cucumber-reports/cucumber-html-report",
+		})
 public class ParallelRunner extends AbstractTestNGCucumberTests {
+	@Test(groups="cucumber",description="Runs cucumber scenarios", dataProvider="scenarios")
+	public void runScenario(PickleWrapper pickleWrapper, FeatureWrapper featureWrapper) throws Throwable{
+		super.runScenario(pickleWrapper, featureWrapper);
+	}
 
 	@Override
-	@DataProvider(parallel = true)
+	@DataProvider(name="scenarios",parallel = true)
 	public Object[][] scenarios() {
 		return super.scenarios();
 	}
