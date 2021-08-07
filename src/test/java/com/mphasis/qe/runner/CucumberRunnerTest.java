6c4e351ff81e0912e69c1911103327a521b0178a
voiceofpanky <voiceofpanky@gmail.com>
2021-08-06 17:49:30 -0700

    runner file update

--- src/test/java/com/mphasis/qe/runner/CucumberRunnerTest.java
+++ src/test/java/com/mphasis/qe/runner/CucumberRunnerTest.java
@@ -1,35 +1,32 @@
-//package com.mphasis.qe.runner;
-//
-//import io.cucumber.junit.Cucumber;
-//import io.cucumber.junit.CucumberOptions;
-//import org.junit.runner.RunWith;
-//
-///****************************************************************************************
-// * @author manoj chavan
-// ****************************************************************************************/
-////@Slf4j
-//@RunWith(Cucumber.class)
-//@CucumberOptions(
-//        features = {"src/test/resources/featurefiles"},
-//        glue = {"com.mphasis.qe.utils",
-//                "com.mphasis.qe.stepdefs"},
-//
-//        //tags = {"not @Ignore"},
-//        tags = {"@fileReader"},
-//        strict = true,
-//
-//        monochrome = true,
-//        dryRun = false,
-//        plugin = {"pretty",
-//                "json:target/cucumber-reports/cucumber.json",
-//                "html:target/cucumber-reports/cucumber-html-report",
-//        })
-//public class CucumberRunnerTest{
-//
-//        }
+package com.mphasis.qe.runner;
 
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
 
+/****************************************************************************************
+ * @author manoj chavan
+ ****************************************************************************************/
+//@Slf4j
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = {"src/test/resources/featurefiles"},
+        glue = {"com.mphasis.qe.utils",
+                "com.mphasis.qe.stepdefs"},
 
+        //tags = {"not @Ignore"},
+        tags = {"@ios"},
+        strict = true,
+
+        monochrome = true,
+        dryRun = false,
+        plugin = {"pretty",
+                "json:target/cucumber-reports/cucumber.json",
+                "html:target/cucumber-reports/cucumber-html-report",
+        })
+public class CucumberRunnerTest{
+
+        }
 //public class CucumberRunnerTest{
 //
 //	public static Map<Integer, String> categoryMap = new HashMap<Integer, String>();
