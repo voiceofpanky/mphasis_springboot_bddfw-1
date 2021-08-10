f62b7c4f510aff4d4406e4219191a7248c11baa7
tamilselvanramalingam <adroittamil@gmail.com>
2021-08-09 22:23:57 -0400

    For Review comment 30

--- src/test/java/com/mphasis/qe/stepdefs/MakePayementSteps.java
+++ src/test/java/com/mphasis/qe/stepdefs/MakePayementSteps.java
@@ -7,7 +7,7 @@ import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
-import org.testng.Assert;
+import org.junit.Assert;
 
 /****************************************************************************************
  * @author Tamilselvan Ramalingam
