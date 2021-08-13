cf04b838a71c840dc0b776f4e2fcc73395e4138b
K Prasad <tokanhaiyap@gmail.com>
2021-08-12 23:09:16 -0500

    Implemented retest/retry logic

---
+++ src/test/java/com/mphasis/qe/runner/CucumberRunnerRetry.java
@@ -0,0 +1,28 @@
+package com.mphasis.qe.runner;
+import com.github.mkolisnyk.cucumber.runner.ExtendedCucumber;
+import com.github.mkolisnyk.cucumber.runner.ExtendedCucumberOptions;
+import cucumber.api.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(ExtendedCucumber.class)
+@ExtendedCucumberOptions(jsonReport = "target/cucumber.json",
+        retryCount = 3,
+        detailedReport = true,
+        detailedAggregatedReport = true,
+        overviewReport = true,
+        //coverageReport = true,
+        jsonUsageReport = "target/cucumber-usage.json",
+        usageReport = true,
+        toPDF = true,
+        outputFolder = "target")
+@CucumberOptions
+        (plugin = { "html:target/cucumber-html-report",
+        "json:target/cucumber.json", "pretty:target/cucumber-pretty.txt",
+        "usage:target/cucumber-usage.json", "junit:target/cucumber-results.xml" },
+        features = {"src/test/resources/featurefiles"},
+        glue = {"com.mphasis.qe.utils",
+                "com.mphasis.qe.stepdefs"}
+        )
+public class CucumberRunnerRetry {
+}
+
--- src/test/java/com/mphasis/qe/runner/CucumberRunnerTestngReRun.java
+++ src/test/java/com/mphasis/qe/runner/CucumberRunnerTestngReRun.java
@@ -4,10 +4,11 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 /****************************************************************************************
- * @author Pankaj Sao
+ * @author Kanhaiya Prasad
  ****************************************************************************************/
 @CucumberOptions(
-		features = {"src/test/resources/featurefiles"},
+		//features = {"src/test/resources/featurefiles"},
+		features ="@target/rerun.txt",
 		glue = {"com.mphasis.qe.utils",
 		"com.mphasis.qe.stepdefs"},
 		tags = {"@web and @fundTransfer"},
@@ -15,10 +16,9 @@ import io.cucumber.testng.CucumberOptions;
 		monochrome = true,
 		dryRun = false,
 		plugin = {"pretty",
-				"json:target/cucumber-reports/cucumber.json",
-				"html:target/cucumber-reports/cucumber-html-report",
-				"rerun:target/rerun.txt",
+				"json:target1/cucumber-reports/cucumber.json",
+				"html:target1/cucumber-reports/cucumber-html-report",
 		})
-public class CucumberRunnerTestng extends AbstractTestNGCucumberTests {
+public class CucumberRunnerTestngReRun extends AbstractTestNGCucumberTests {
 }
 
